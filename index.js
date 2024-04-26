3535a6ef6e320e4ce827455d89eb0fbf223ec911
nikisathe <nikitasathe1734@gmail.com>
2024-04-26 15:44:56 +0530

    f commit

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const mysql = require('mysql');
 const app = express();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'college'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT // Add this line to specify the database port
 });
 
 db.connect((err) => {
@@ -18,7 +19,7 @@ db.connect((err) => {
 });
 
 app.get('/api/data', (req, res) => {
-    const sql = 'SELECT * FROM college_list';
+    let sql = 'SELECT * FROM college_list';
     db.query(sql, (err, result) => {
         if(err) {
             throw err;
